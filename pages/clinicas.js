acb6334ff9b3477b505df50770ceaeef2f29d3db
diegomendezp <diegomen15@gmail.com>
2019-06-03 20:01:03 +0200

    Clinicas page carrousel ✨

--- pages/clinicas.js
+++ pages/clinicas.js
@@ -9,6 +9,13 @@ import img3 from '../static/imgs/clinicas/galeria/imgclinicagaleria6.jpg'
 import img4 from '../static/imgs/clinicas/mapa.jpg'
 import img5 from '../static/imgs/clinicas/marker.png'
 import img6 from '../static/imgs/clinicas/galeria/imgclinicagaleria9.jpg'
+
+import image1 from "../static/imgs/clinicas/galeria/imgclinicagaleria1.jpg";
+import image3 from "../static/imgs/clinicas/galeria/imgclinicagaleria7.jpg";
+import image4 from "../static/imgs/clinicas/galeria/imgclinicagaleria5.jpg";
+import image2 from "../static/imgs/clinicas/galeria/imgclinicagaleria2.jpg";
+import image5 from "../static/imgs/clinicas/galeria/imgClinica2.jpg";
+import image6 from "../static/imgs/clinicas/galeria/imgclinicagaleria8.jpg";
 import Avenir from "../static/fonts/avenir.ttf";
 import AvenirL from "../static/fonts/Avenir-Light-07.ttf";
 import { ImgWithText } from '../components/pageSections/ImgWithText';
@@ -19,14 +26,20 @@ import { ClinicasMainPageStyle } from '../PagesStyles/ClinicasMainPageStyle'
 import ImageGallery from 'react-image-gallery';
 
 const images = [
-    {original: img3},
-    {original: img2}
+    {original: image1},
+    {original: image3},
+    {original: image4},
+    {original: image2},
 ]
 
 const texto = {
-    p1: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam tristique ullamcorper massa, nec tincidunt diam egestas vitae. Ut a elit lorem. Nulla ultricies erat vitae varius mattis. Etiam blandit lacus purus, eu rhoncus dolor sollicitudin vitae. Ut vel vestibulum nulla. Cras dui nibh, mattis et sollicitudin quis, pharetra faucibus urna. ",
-    p2: "Aenean condimentum interdum tortor a porta. Nulla eu diam sodales, facilisis est a, laoreet risus. Fusce iaculis quam commodo felis ornare, in tincidunt turpis fermentum. Vivamus sed augue aliquet, mattis neque eu, tincidunt massa. Proin iaculis sapien vel maximus interdum. In hac habitasse platea dictumst. Morbi pretium lorem vitae pretium cursus. Morbi mattis, quam id porta maximus, arcu dui vestibulum ipsum, quis molestie ligula lectus eu metus. Integer sit amet nisl sed magna venenatis vehicula. Cras sed dolor eget orci fringilla rutrum at faucibus nulla. ",
-    p3: "Duis tempor nulla erat, nec vulputate turpis placerat vitae. Etiam vel pretium augue. Ut vel aliquet purus. Etiam ut fringilla ante. Suspendisse rhoncus ultrices lacus nec tempor.",
+    p1: "Clínica Cidad inició su actividad al cuidado de la salud y de la estética hace más de 30 años con un solo objetivo: ofrecer un servicio de excelencia sumado a una atención personalizada.",
+    p2: "Con este propósito y decididos a renovarnos queremos presentarles la nueva clínica Cidad, situada en el barrio de Hortaleza. Ubicada en un espacio singular cuyo diseño orientado al bienestar de nuestros pacientes, se caracteriza por la luz, que invade todas las estancias. Cada rincón de la clínica está minuciosamente diseñado para transportarnos a un paisaje natural.",
+    p3: "Una de las principales novedades en el diseño de la clínica es el espacio dedicado a la sala de espera que se divide en dos zonas, una en forma de terraza chillout donde disfrutar del buen tiempo y otra interior planteada para un uso polivalente, conferencias, exposiciones, cursos…",
+}
+
+const texto2 = {
+    p1: "Contamos con una totalidad de 7 gabinetes entre ambas clínicas dotados con las últimas tecnologías y radiodiagnóstico que garantiza el éxito de nuestros procedimientos, además todos los gabinetes y ambas clínicas están conectadas informáticamente facilitando la comunicación interpersonal y el trabajo colaborativo entre el equipo multidisciplinar; la suma de los conocimiento de cada especialista son la clave del éxito de nuestros tratamientos."
 }
 
 const listTratamientos = ["odontología preventiva", "Odontopediatría", "Eestética dental"]
@@ -74,10 +87,10 @@ const Clinicas = props => {
                     <ImgWithText img={img2} width={50} text={"Machupichu 25 |  Alcalá 590 1A"} colorText={"black"} imgPosition={-60} order={3}></ImgWithText>
                     <TextSection text={texto}></TextSection>
                 </div>
-                <ImageGallery items={images} showThumbnails={false} showPlayButton={false}/>
+                <ImageGallery items={images} showThumbnails={false} showPlayButton={false} showNav={false} autoPlay={true} slideDuration={600} lazyLoad={true} />
                 
                 <div className="secondSectionClinicas">
-                    <TextSection text={texto}></TextSection>
+                    <TextSection text={texto2}></TextSection>
                     <div className="mapContainer">
                         <img src={img5} className="marker"></img>
                         <img src={img4} className="imagenMapa"></img>
