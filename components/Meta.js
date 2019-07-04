91436ac6c3ea7a3377309bcc1ce1bc825a224abe
diegomendezp <diegomen15@gmail.com>
2019-07-04 16:00:35 +0200

    Meta component created & added to home page

---
+++ components/Meta.js
@@ -0,0 +1,53 @@
+import Head from "next/head";
+
+const Meta = ({ title }) => {
+  return (
+    <React.Fragment>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charSet="utf-8" />
+        <title>{"Clínica Cidad"}</title>
+      </Head>
+      <style jsx global>{`
+        @font-face {
+          font-family: "Avenir";
+          src: url(/static/fonts/avenir.ttf) format("truetype");
+          font-weight: normal;
+          font-style: normal;
+        }
+
+        @font-face {
+          font-family: "AvenirB";
+          src: url(/static/fonts/Avenir-bold.ttf) format("truetype");
+          font-weight: normal;
+          font-style: normal;
+        }
+
+        @font-face {
+          font-family: "AvenirL";
+          src: url(/static/fonts/Avenir-Light-07.ttf) format("truetype");
+          font-weight: normal;
+          font-style: normal;
+        }
+
+        body {
+          margin: 0;
+          padding: 0;
+          max-width: 100%;
+          position: relative;
+          overflow-x: hidden;
+        }
+
+        .center {
+          width: 100vw;
+          height: auto;
+          display: flex;
+          justify-content: center;
+          text-align: center;
+        }
+      `}</style>
+    </React.Fragment>
+  );
+};
+
+export default Meta;
--- pages/index.js
+++ pages/index.js
@@ -1,67 +1,21 @@
 import React from "react";
-import Head from "next/head";
 import NavBar from "../components/NavBar";
 import Main from "../components/Main";
 import Tratamientos from "../components/Tratamientos";
 import Equipo from "../components/Equipo";
 import Footer from "../components/Footer";
-import Avenir from "../static/fonts/avenir.ttf";
-import AvenirB from "../static/fonts/Avenir-bold.ttf";
-import AvenirL from "../static/fonts/Avenir-Light-07.ttf";
-import imageOffice from "../static/imgs/home/imgHome1.jpg";
 import Cookies from "../components/Cookies/Cookies";
 import FloatingMenu from "../components/FloatingMenu";
+import Meta from "../components/Meta";
 
 export default () => (
   <React.Fragment>
-    <Head>
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <meta charSet="utf-8" />
-      <title>Clínica Cidad</title>
-    </Head>
-    <style jsx global>{`
-      @font-face {
-        font-family: "Avenir";
-        src: url(${Avenir}) format("truetype");
-        font-weight: normal;
-        font-style: normal;
-      }
-
-      @font-face {
-        font-family: "AvenirB";
-        src: url(${AvenirB}) format("truetype");
-        font-weight: normal;
-        font-style: normal;
-      }
-
-      @font-face {
-        font-family: "AvenirL";
-        src: url(${AvenirL}) format("truetype");
-        font-weight: normal;
-        font-style: normal;
-      }
-
-      body {
-        margin: 0;
-        padding: 0;
-        max-width: 100%;
-        position: relative;
-        overflow-x: hidden;
-      }
-
-      .center {
-        width: 100vw;
-        height: auto;
-        display: flex;
-        justify-content: center;
-        text-align: center;
-      }
-    `}</style>
+    <Meta />
     <React.Fragment>
     <FloatingMenu />
       <NavBar />
       <Main home
-        img={imageOffice}
+        img={"/static/imgs/home/imgHome1.jpg"}
         imgPosition={-251}
         title={"Clínica Cidad"}
         subtitle={"odontología, ortodoncia y medicina estética."}
