66448fe337bc275b1f5e83812baa2fbb4bed1bcb
diegomendezp <diegomen15@gmail.com>
2019-07-04 15:25:42 +0200

    Treatments list medias

--- PagesStyles/ListItemsStyle.js
+++ PagesStyles/ListItemsStyle.js
@@ -1,34 +1,115 @@
 import styled from 'styled-components';
 
 const ListItemsWrapperStyle = styled.div`
-  display:flex;
+  display: flex;
+  width: 100%;
   justify-content: center;
   align-items: center;
-  width:70vw;
-  margin: 0 auto;
-  height:50vh;
-  div{
-    width:30vw;
+  height: 50vh;
 
-  }
-  p,h3 {
-      font-family: "AvenirL";
+  p {
+    margin-top: 0;
   }
 
-  .treatment-header{
+  .treatments {
     display: flex;
-    img {
-      margin-right: 15px;
+    justify-content: space-between;
+    align-items: flex-start;
+    width: 80vw;
+    margin: 0 auto;
+
+    .treatments-container {
+      width: 30%;
+    }
+    p,
+    h3 {
+      font-family: "AvenirL";
+    }
+
+    .treatment-header {
+      display: flex;
+      img {
+        margin-right: 15px;
+      }
     }
   }
-  
 
+  @media (min-width: 1441px) {
+    height: 80vh;
+    p,
+    h3 {
+      font-size: 1.25vw;
+    }
+  }
 
+  @media (min-width: 1025px) and (max-width: 1440px) {
+    height: 80vh;
+    p,
+    h3 {
+      font-size: 1.5vw;
+    }
+  }
 
+  @media (min-width: 769px) and (max-width: 1024px) {
+    height: auto;
+    .treatments {
+      padding: 6% 0;
+      flex-direction: column;
 
+      .treatments-container {
+        width: 100%;
+      }
+    }
+    p,
+    h3 {
+      font-size: 2.5vw;
+    }
+    width: 100vw;
+    order: 0;
+  }
 
+  @media (min-width: 415px) and (max-width: 768px) {
+    height: auto;
+    .treatments {
+      padding: 8% 0;
+      flex-direction: column;
 
+      .treatments-container {
+        width: 100%;
+      }
+    }
+    p,
+    h3 {
+      font-size: 2.5vw;
+    }
+    width: 100vw;
+    order: 0;
+  }
 
+  @media (max-width: 414px) {
+    p {
+      margin: 2% 0;
+    }
+    height: auto;
+    .treatments {
+      padding: 10% 0;
+      flex-direction: column;
+
+      .treatments-container {
+        width: 100%;
+      }
+
+      .treatment-header {
+        margin-top: 5%;
+      }
+    }
+    p,
+    h3 {
+      font-size: 3.5vw;
+    }
+    width: 100vw;
+    order: 0;
+  }
 `;
 
 export { ListItemsWrapperStyle };
--- components/pageSections/ListItems.js
+++ components/pageSections/ListItems.js
@@ -4,39 +4,50 @@ import {ListItemsWrapperStyle} from '../../PagesStyles/ListItemsStyle'
 const ListItems = props => {
     return (
         <ListItemsWrapperStyle>
-            <div>
-                <div className="treatment-header">
-                    <img src="/static/imgs/svg/Preventiva.svg" alt="odontologia-icon"/>
-                    <h3>ODONTOLOGÍA PREVENTIVA</h3>
-                </div>
-                <p>– De caries</p>
-                <p>– De enfermedad periodontal e Higiene</p>
-                <div className="treatment-header">
-                    <img src="/static/imgs/svg/Odontopediatría.svg" alt="odontopedriatia-icon"/>
-                    <h3>ODONTOPEDIATRÍA</h3>
-                </div>
-                <div className="treatment-header">
-                    <img src="/static/imgs/svg/Endodoncia.svg" alt="endodoncia-icon"/>
-                    <h3>ENDODONCIA</h3>
-                </div>
-                <div className="treatment-header">
-                    <img src="/static/imgs/svg/Implantología.svg" alt="implantologia-icon"/>
-                    <h3>IMPLANTOLOGÍA</h3>
-                </div>
-            </div>               
-            <div>
-                <h3>PERIODONCIA</h3>
-                <p>– Aparatología removible</p>
-                <p>– Apartología fija"</p>
-                <p>– Brackets Damon System ® (meátilocs / Estéticos)</p>
-                <p>– Prótesis (Fija / Removible)</p>
-                <p>– Ortodoncia invisible. Ivisaling®</p>
-            </div> 
-            <div>
-                <h3>MAXILIFACIAL</h3>
-                <h3>RADIODIAGNÓSTICO 3D</h3>
-                <p>– Limpieza con aeropulidor</p>
-            </div> 
+            <div className="treatments">
+                <div className="treatments-container">
+                    <div className="treatment-header">
+                        <img src="/static/imgs/svg/Preventiva.svg" alt="odontologia-icon"/>
+                        <h3>ODONTOLOGÍA PREVENTIVA</h3>
+                    </div>
+                    <p>– De caries</p>
+                    <p>– De enfermedad periodontal e Higiene</p>
+                    <div className="treatment-header">
+                        <img src="/static/imgs/svg/Odontopediatría.svg" alt="odontopedriatia-icon"/>
+                        <h3>ODONTOPEDIATRÍA</h3>
+                    </div>
+                    <div className="treatment-header">
+                        <img src="/static/imgs/svg/Endodoncia.svg" alt="endodoncia-icon"/>
+                        <h3>ENDODONCIA</h3>
+                    </div>
+                    <div className="treatment-header">
+                        <img src="/static/imgs/svg/Implantología.svg" alt="implantologia-icon"/>
+                        <h3>IMPLANTOLOGÍA</h3>
+                    </div>
+                </div>               
+                <div className="treatments-container">
+                    <div className="treatment-header">
+                        <img src="/static/imgs/svg/Perodoncia.svg" alt="perodoncia-icon"/>
+                        <h3>PERODONCIA</h3>
+                    </div>
+                    <p>– Aparatología removible</p>
+                    <p>– Apartología fija"</p>
+                    <p>– Brackets Damon System ® (meátilocs / Estéticos)</p>
+                    <p>– Prótesis (Fija / Removible)</p>
+                    <p>– Ortodoncia invisible. Ivisaling®</p>
+                </div> 
+                <div className="treatments-container">
+                    <div className="treatment-header">
+                        <img src="/static/imgs/svg/Maxilofacial.svg" alt="maxilofacial-icon"/>
+                        <h3>MAXILOFACIAL</h3>
+                    </div>
+                    <div className="treatment-header">
+                        <img src="/static/imgs/svg/Radiodiagnostico.svg" alt="readiodiagnostico-icon"/>
+                        <h3>RADIODIAGNÓSTICO 3D</h3>
+                    </div>
+                    <p>– Limpieza con aeropulidor</p>
+                </div> 
+            </div>
 
         </ListItemsWrapperStyle>
     )
