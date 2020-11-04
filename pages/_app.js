dcfacac9b25e39ac838341b5fbcf3602b04e4e53
Diego Méndez <diegomendezp@MacBook-Pro-de-Diego.local>
2020-11-04 17:16:24 +0100

    feat: Google Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,28 @@
-import App, { Container } from 'next/app'
-import React from 'react'
+import { Container } from 'next/app'
+import React, { useEffect } from 'react'
 import "react-image-gallery/styles/css/image-gallery.css";
 import Router from "next/router";
+import * as gtag from '../lib/gtag';
+import { useRouter } from 'next/router'
 
-export default class MyApp extends App {
-  render () {
-    const { Component, pageProps } = this.props
+export default function App({ Component, pageProps }) {
+    const router = useRouter()
+    useEffect(() => {
+        const handleRouteChange = (url) => {
+            gtag.pageview(url)
+        }
+        router.events.on('routeChangeComplete', handleRouteChange)
+        return () => {
+            router.events.off('routeChangeComplete', handleRouteChange)
+        }
+    }, [router.events])
+
     return (
-      <Container>
-          <Component {...pageProps} />
-      </Container>
+        <Container>
+            <Component {...pageProps} />
+        </Container>
     )
-  }
+
 }
 initRouterListeners();
 
@@ -27,14 +38,14 @@ function initRouterListeners() {
     });
 
     function pushCurrentRouteInfo() {
-        routes.push({pathname: Router.pathname, scrollY: window.scrollY});
+        routes.push({ pathname: Router.pathname, scrollY: window.scrollY });
     }
 
     function isBack() {
         return routes.length >= 2 && Router.pathname === routes[routes.length - 2].pathname;
     }
 
-    function fixScrollPosition () {
+    function fixScrollPosition() {
 
         let scrollY = 0;
 
--- pages/_document.js
+++ pages/_document.js
@@ -1,16 +1,44 @@
-import Document from 'next/document'
-import { ServerStyleSheet } from 'styled-components'
-
+import Document from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
+import { GA_TRACKING_ID } from '../lib/gtag';
+import Head from 'next/head';
 
 export default class MyDocument extends Document {
-  static async getInitialProps (ctx) {
+  static async getInitialProps(ctx) {
     const sheet = new ServerStyleSheet()
     const originalRenderPage = ctx.renderPage
 
     try {
       ctx.renderPage = () =>
         originalRenderPage({
-          enhanceApp: App => props => sheet.collectStyles(<App {...props} />)
+          enhanceApp: App => props => sheet.collectStyles(
+            <html lang='es'>
+              <Head>
+                {/* Global Site Tag (gtag.js) - Google Analytics */}
+                {process.env.NODE_ENV === 'production' && (
+                  <>
+                    <script
+                      async
+                      src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+                    />
+                    <script
+                      dangerouslySetInnerHTML={{
+                        __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+                      }}
+                    />
+                  </>
+                )}
+              </Head>
+              <App {...props} />
+            </html>
+          )
         })
 
       const initialProps = await Document.getInitialProps(ctx)
